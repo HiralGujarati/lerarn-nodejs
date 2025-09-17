a5afdfe34fb345184c901b4a470db41f9261c0b9
HiralGujarati <hiralgujarati6539@gmail.com>
2025-09-17 18:16:13 +0530

    cors error from frontend

--- index.js
+++ index.js
@@ -1,73 +1,13 @@
-import mongoose from "mongoose";
 import express from "express";
-import studentModel from "./models/studentModel.js";
+import cors from 'cors';
+
 const app = express();
-app.use(express.json());
-await mongoose.connect("mongodb://localhost:27017/collage").then(() => {
-  console.log("______________mongoose connect____________")
-});
-
-app.get("/", async (req, res) => {
-  const studentData = await studentModel.find()
-  res.send(studentData);
-});
-
-app.post("/save", async (req, res) => {
-  console.log(req.body);
-  const studentData = await studentModel.create(req.body);
-  console.log(studentData);
-  const { name, age, email } = req.body;
-  if (!req.body || !name || !age || !email) {
-    res.send({
-      message: "data not saved",
-      success: false,
-      storedInfo: null
-    })
-  }
-
+app.use(cors());
+app.get("/", (req, res) => {
   res.send({
-    message: "data saved",
-    success: true,
-    storedInfo: studentData
-  })
-});
-app.put("/update/:id", async (req, res) => {
-  const id = req.params.id;
-  console.log(req.body, id);
-  const studentData = await studentModel.findByIdAndUpdate(id, { ...req.body })
-  res.send({
-    message: "data updated",
-    success: true,
-    info: studentData
-  })
-
-})
-app.delete("/delete/:id", async (req, res) => {
-  const id = req.params.id;
-  console.log(req.body, id);
-  const studentData = await studentModel.findByIdAndDelete(id, { ...req.body });
-  console.log(studentData);
-  res.send({
-    message: "data deleted",
-    success: true,
-    info: studentData
+    "name": "hiral",
+    "age": 19,
+    "email": "hiral@gmail.com"
   });
-})
-
-app.listen(3200);
-
-// async function dbConnections() {
-//   await mongoose.connect("mongodb://localhost:27017/collage")
-//   const schema = mongoose.Schema({
-//     name: String,
-//     age: Number,
-//     email: String
-//   })
-//   const studentsModel = mongoose.model('students', schema);
-//   const result = await studentsModel.find();
-//   console.log(result);
-
-
-// };
-// dbConnections();
-
+});
+app.listen(3200)
--- models/studentModel.js
+++
@@ -1,6 +0,0 @@
-import mongoose from "mongoose";
-import studentSchema from "../schemas/studentSchema.js";
-
- const studentModel = mongoose.model("student", studentSchema)
-
- export default studentModel;
